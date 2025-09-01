24e5ffbb8c45a2c5941eb9567beb15cc2c83fdb8
Xavier Valles <xvv1988@gmail.com>
2025-09-01 22:57:00 +0200

    Configure Vite to deploy

--- frontend-blog/vite.config.js
+++ frontend-blog/vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'  // mantén este plugin
 
 export default defineConfig({
-  base: '/',
+  base: '/BlogTheChollos/',
   plugins: [
     tailwindcss(),
     react()
